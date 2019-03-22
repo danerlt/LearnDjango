f3312f0649f194889715bb915613f3cbc838682a
danerlt <danerlt001@gmail.com>
2019-03-22 17:55:07 +0800

    form表单demo

--- student_sys/student/views.py
+++ student_sys/student/views.py
@@ -1,3 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from __future__ import unicode_literals
+
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Student
+from .forms import StudentForm
+
+
+def index(request):
+    students = Student.objects.all()
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+            student = Student()
+            student.name = cleaned_data['name']
+            student.sex = cleaned_data['sex']
+            student.email = cleaned_data['email']
+            student.profession = cleaned_data['profession']
+            student.qq = cleaned_data['qq']
+            student.phone = cleaned_data['phone']
+            student.save()
+            return HttpResponseRedirect(reverse('index'))
+    else:
+        form = StudentForm()
+
+    context = {
+        'students': students,
+        'form': form,
+    }
+    return render(request, 'index.html', context=context)
--- student_sys/student_sys/urls.py
+++ student_sys/student_sys/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from student.views import index
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url('^$', index, name="index")
 ]
