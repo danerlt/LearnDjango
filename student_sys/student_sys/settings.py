b2c1526ef6586ed58d7db3ec9d7c2cf2fee3e6d6
danerlt <danerlt001@gmail.com>
2019-03-27 09:22:40 +0800

    add middlewares

--- student_sys/student_sys/settings.py
+++ student_sys/student_sys/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'student.middlewares.TimeItMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
