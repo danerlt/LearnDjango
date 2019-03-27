e77d34a27856e9d010cba03002ed73bfe308e314
danerlt <danerlt001@gmail.com>
2019-03-27 10:46:13 +0800

    add unittest

--- student_sys/student/tests.py
+++ student_sys/student/tests.py
@@ -1,3 +1,54 @@
-from django.test import TestCase
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-# Create your tests here.
+from django.test import TestCase, Client
+
+from .models import Student
+
+
+class StudentTestCase(TestCase):
+    def setUp(self):
+        Student.objects.create(
+            name='test',
+            sex=1,
+            email='333@dd.com',
+            profession='程序员',
+            qq='3333',
+            phone='32222',
+        )
+
+    def test_create(self):
+        student = Student.objects.create(
+            name='test',
+            sex=1,
+            email='333@dd.com',
+            profession='程序员',
+            qq='3333',
+            phone='32222',
+        )
+        self.assertNotEqual(student, None, 'student test_create success')
+
+    def test_filter(self):
+        students = Student.objects.filter(name='test')
+        self.assertEqual(students.count(), 1, 'only one is right')
+
+    def test_get_index(self):
+        client = Client()
+        response = client.get('/')
+        self.assertEqual(response.status_code, 200, 'status code must be 200!')
+
+    def test_post_student(self):
+        client = Client()
+        data = dict(
+            name='test_for_post',
+            sex=1,
+            email='333@dd.com',
+            profession='程序员',
+            qq='3333',
+            phone='32222',
+        )
+        response = client.post('/', data)
+        self.assertEqual(response.status_code, 302, 'status code must be 302!')
+
+        response = client.get('/')
+        self.assertTrue(b'test_for_post' in response.content, 'response content must contain `test_for_post`')
